4d52ee3045cdbac76d51372c6d9c6c5eef4e97a3
velasciraptor <amyvelasquez432@gmail.com>
2021-10-19 19:05:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 # set up for SQLite
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
@@ -31,4 +36,10 @@ def hello_world():
     /api/v1.0/temp/start/end
     ''')
 
-
+# create precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+   return jsonify(precip)
